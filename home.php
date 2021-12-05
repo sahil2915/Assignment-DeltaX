2ce6e46f79b8c34bf4b135294bc6827165bd9feb
sahil2915 <95564294+sahil2915@users.noreply.github.com>
2021-12-05 17:46:32 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale= 1.0">
+	<meta http-equiv="X-UA-compatible" content="IE=edge">
+	<meta name="author" content="Sahil Sinha">
+	<meta name="description" content="">
+	<meta name="keyword" content="">
+	<meta name="robot" content="INDEX,FOLLOW">
+
+
+	<title></title>
+         
+       
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+<!--	<link rel="stylesheet" type="text/css" media="screen and (min-width:) and (max-width:)" href="responsive.css"> -->
+
+        <script type="text/javascript" src="dynamic.js"></script>
+</head>
+<body>
+
+	
+
+   <nav>
+          <img src="logo.png">
+
+        <ul>
+                
+                <li>
+                        <a href="#">Get a Quote</a>
+                </li>
+        </ul>
+
+        <ul>
+                <li>
+                        <a href="#">Change Policy</a>
+                </li>
+        </ul>
+
+        <ul>
+                <li>
+                        <a href="#"> Clams</a>
+                </li>
+        </ul>
+         
+
+         <ul>
+                 <li>
+                         <a href="#">contacts</a>
+                 </li>
+         </ul>
+        
+   </nav>
+    
+    <div id= search>
+
+        <span>
+            <h1>Sk Finance </h1>
+        </span>
+
+        <input type="search" name="searchengine" placeholder="search" size="50">
+        <button onclick="fun()">Find</button>
+    </div> 
+
+     <div id="insurance">
+        <div class="item">
+            <span>Life Insurance</span>
+        </div>
+
+        <div class="item">
+            <span>Health</span>
+        </div>
+
+        <div class="item">
+            <span>Bike/Car</span>
+        </div>
+
+        <div class="item">
+            <span>Loan</span>
+        </div>
+    </div>
+    
+    <script >
+        function fun() {
+            // body...
+            alert("you click");
+            console.log("sahil")
+        }
+    </script>
+
+    <!--<div>
+          <h1>skslsl<h1>
+    </div>-->
+</body>
+</html>
